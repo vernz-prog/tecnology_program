649e5da872824060c529588e2ebfc133ed7c57e5
vasil <vasilcenkodanii21@gmail.com>
2024-06-02 14:16:52 +0300

    laba2

--- src/Mypanal.java
+++ src/Mypanal.java
@@ -1,23 +1,190 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util. ArrayList;
+
 public class MyPanel extends JPanel {
     public MyPanel() {
+
         setBorder(BorderFactory.createLineBorder(Color.black));
     }
-
-    public Dimension getPreferredSize() {
-        return new Dimension(1560, 700);
-    }
-
-    private int x = 0;
-    private int y = 0;
-    private int width = 100;
+    public Dimension getPreferredSize() { return new
+            Dimension(1560, 700); }
+    private int x = 100;
+    private int y = 100;
+    private int width = 50;
     private int height = 100;
-    private String[] numbers = null;
-    private int otstup = 1;
-
+    private int step = 30;
+    private int delta = 5;
+    private ArrayList<Rectangle> outlist = new
+            ArrayList<Rectangle>();
+    public void setSymbols(String parm) {
+        String[] arr = parm.replace(",", "").split("");
+        for (String cifra : arr) {
+            switch (cifra) {
+                case "0":
+                    segment(1);
+                    segment(2);
+                    segment(4);
+                    segment(5);
+                    segment(6);
+                    segment(7);
+                    break;
+                case "1":
+                    segment(2);
+                    segment(4);
+                    break;
+                case "2":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(5);
+                    segment(6);
+                    break;
+                case "3":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(4);
+                    segment(5);
+                    break;
+                case "4":
+                    segment(2);
+                    segment(3);
+                    segment(4);
+                    segment(7);
+                    break;
+                case "5":
+                    segment(1);
+                    segment(3);
+                    segment(4);
+                    segment(5);
+                    segment(7);
+                    break;
+                case "6":
+                    segment(1);
+                    segment(3);
+                    segment(4);
+                    segment(5);
+                    segment(6);
+                    segment(7);
+                    break;
+                case "7":
+                    segment(1);
+                    segment(2);
+                    segment(4);
+                    break;
+                case "8":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(4);
+                    segment(5);
+                    segment(6);
+                    segment(7);
+                    break;
+                case "9":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(4);
+                    segment(5);
+                    segment(7);
+                    break;
+                case "Д":
+                    segment(1);
+                    segment(7);
+                    segment(6);
+                    segment(4);
+                    segment(5);
+                    segment(2);
+                    segment(8);
+                    segment(9);
+                    segment(10);
+                    segment(11);
+                    break;
+                case "И":
+                    segment(2);
+                    segment(4);
+                    segment(6);
+                    segment(7);
+                    segment(13);
+                    segment(14);
+                    break;
+                case "М":
+                    segment(2);
+                    segment(4);
+                    segment(6);
+                    segment(7);
+                    segment(12);
+                    segment(13);
+                    break;
+                case "А":
+                    segment(1);
+                    segment(2);
+                    segment(3);
+                    segment(4);
+                    segment(6);
+                    segment(7);
+                    break;
+            }
+            x = x + step + width;
+        }
+    }
+    private void segment(int number){
+        switch(number){
+            case 1:outlist.add(new Rectangle(x+delta,y,
+                    x+width-delta,y));
+                break;
+            case 2:outlist.add(new Rectangle(x+width,
+                    y+delta, x+width, y+(height/2)-delta));
+                break;
+            case 3:outlist.add(new Rectangle(x+delta,
+                    y+height/2, x+width-delta, y+height/2));
+                break;
+            case 4:outlist.add(new Rectangle(x+width,
+                    y+(height/2)+delta, x+width, y+height-delta));
+                break;
+            case 5:outlist.add(new Rectangle(x+delta,
+                    y+height, x+width-delta, y+height));
+                break;
+            case 6:outlist.add(new Rectangle(x,
+                    y+(height/2)+delta, x, y+height-delta));
+                break;
+            case 7:outlist.add(new Rectangle(x,
+                    y+delta, x, y+(height/2)-delta));
+                break;
+            case 8:outlist.add(new Rectangle(x+width,
+                    y+height, x+width+delta*2,y+height));
+                break;
+            case 9:outlist.add(new
+                    Rectangle(x+width+delta*2, y+height,
+                    x+width+delta*2,y+height+delta*2));
+                break;
+            case 10:outlist.add(new Rectangle(x,
+                    y+height, x-delta*2,y+height));
+                break;
+            case 11:outlist.add(new Rectangle(x-
+                    delta*2, y+height, x-delta*2,y+height+delta*2));
+                break;
+            case 12:outlist.add(new
+                    Rectangle(x+width/2-delta, y+height/2-delta, x+delta,
+                    y+delta));
+                break;
+            case 13:outlist.add(new
+                    Rectangle(x+width/2+delta/2, y+height/2-delta, x+width-
+                    delta, y+delta));
+                break;
+            case 14:outlist.add(new Rectangle(x+delta,
+                    y+height-delta, x+width/2-delta/2, y+height/2+delta));
+                break;
+        }
+    }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawLine(x, y, width, height);
+
+        for (Rectangle rect:outlist) {
+            g.drawLine(rect.x, rect.y, rect.width,
+                    rect.height);
+        }
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,14 +1,17 @@
 import javax.swing.*;
 
 public class Main {
+
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
-        System.out.println("Hello, World!");
+        JFrame f = new JFrame("Swing Paint Demo");
 
-        JFrame f = new JFrame( "");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MyPanel myPanel = new MyPanel();
         myPanel.repaint();
+
+        myPanel.setSymbols("0,1,2,3,4,5,6,7,8,9,Д,И,М,А");
         f.add(myPanel);
         f.pack();
         f.setVisible(true);
